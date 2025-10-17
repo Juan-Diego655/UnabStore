ad234231a75420b43cafa309a8e40f55225187e0
Juan Diego Niño Solano <jnino825@unab.edu.co>
2025-10-17 11:15:15 -0500

    Finish up navigation between pages

--- app/src/main/java/me/juandiegoninoosolano/unabstore/MainActivity.kt
+++ app/src/main/java/me/juandiegoninoosolano/unabstore/MainActivity.kt
@@ -21,23 +21,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            val startDestination = "Login"
-            NavHost(
-                navController = navController,
-                startDestination = startDestination,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                composable(route = "Login") {
-                    LoginScreen()
-                }
-                composable (route = "register") {
-                    RegisterScreen()
-                }
-                composable (route = "home") {
-                    HomeScreen()
+            UnabStoreTheme {
+                val navController = rememberNavController()
+                val startDestination = "Login"
+                NavHost(
+                    navController = navController,
+                    startDestination = startDestination,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    composable(route = "Login") {
+                        LoginScreen(onClickRegister = {
+                            navController.navigate("register")
+                        })
+                    }
+                    composable (route = "register") {
+                        RegisterScreen(onClickBack = {
+                            navController.popBackStack()
+                        }
+                        )
+                    }
+                    composable (route = "home") {
+                        HomeScreen()
+                    }
                 }
             }
+
             }
         }
     }
